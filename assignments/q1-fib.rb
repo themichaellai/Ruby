9e5e71be4c6e219dfc608d0c986176b8897f788f
Michael Lai <themichaellai@gmail.com>
2013-01-24 22:46:10 -0500

    q1 and q2

--- assignments/q1-fib.rb
+++ assignments/q1-fib.rb
@@ -1,3 +1,18 @@
 def fib(n, n1 = 1, n2 = 1)
-  # ...
+  if n == 1
+    return n1
+  elsif n == 2
+    return n2
+  else
+    new = n1 + n2
+    n1 = n2
+    n2 = new
+    return fib(n - 1, n1, n2)
+  end
+end
+
+if __FILE__ == $0
+  p fib(1)
+  p fib(6)
+  p fib(11, 0)
 end
--- assignments/q2-sort.rb
+++ assignments/q2-sort.rb
@@ -1,3 +1,41 @@
+def merge(left, right)
+  result = []
+  while left.count > 0 or right.count > 0
+    if left.count > 0 and right.count > 0
+      if (left.first <=> right.first) < 0
+        result.push left.shift
+      else
+        result.push right.shift
+      end
+    elsif left.count > 0
+      result.push left.shift
+    elsif right.count > 0
+      result.push right.shift
+    end
+  end
+  result
+end
+
 def sort(arr)
-  # ...
+  if arr.count <= 1
+    return arr
+  end
+
+  left = []
+  right = []
+  arr[0...arr.count/2].each do |n|
+    left.push n
+  end
+  arr[arr.count/2...arr.count].each do |n|
+    right.push n
+  end
+  left = sort left
+  right = sort right
+  merge left, right
+end
+
+if __FILE__ == $0
+  rand_nums = 10.times.map{ Random.rand(99) }
+  p rand_nums
+  p sort rand_nums
 end
