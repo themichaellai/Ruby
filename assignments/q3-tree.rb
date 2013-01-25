c18e760158c45479a1fc674ce1503d9a26a1cda6
Michael Lai <themichaellai@gmail.com>
2013-01-25 16:39:06 -0500

    q3, better way to pass has to Tree.new?

--- assignments/q3-tree.rb
+++ assignments/q3-tree.rb
@@ -15,3 +15,38 @@ class Tree
     block.call self
   end
 end
+class Tree
+
+  def initialize(tree)
+    @children = []
+    @node_name = tree.keys.first
+    get_children(tree[@node_name])
+  end
+
+  def get_children(hash)
+    hash.keys.each do |key|
+      # better way to do this?
+      @children.push Tree.new({key => hash[key]})
+    end
+  end
+
+  def to_s
+    @node_name
+  end
+end
+
+if __FILE__ == $0
+  tree = Tree.new({
+    'grandpa' => {
+      'dad' => {
+        'child 1' => {},
+        'child 2' => {}
+      },
+      'uncle' => {
+        'child 3' => {},
+        'child 4' => {}
+      }
+    }
+  })
+  tree.visit_all {|node| p "#{node.node_name} #{node.children}"}
+end
