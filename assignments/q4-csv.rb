49f74542c7a52c42c339b82e1ac1df875822890f
Michael Lai <themichaellai@gmail.com>
2013-01-29 15:00:12 -0500

    Q4

--- assignments/q4-csv.rb
+++ assignments/q4-csv.rb
@@ -1,3 +1,4 @@
+# Need error checking?
 module ActsAsCsv
   def self.included(base)
     base.extend ClassMethods
@@ -14,13 +15,41 @@ module ActsAsCsv
     def initialize(rows)
       first, *rest = rows
       @headers = first.split(', ')
-      @csv_contents = rest.map { |row| row.split(', ') }
+      @headers_dict = {}
+      @headers.each_with_index { |header, i| @headers_dict[header.to_sym] = i }
+      @csv_contents = rest.map { |row| CsvRow.new(row.split(', '), @headers_dict) }
     end
   end
 end
 
+class CsvRow
+  def initialize(row, headers_dict)
+    @row = row
+    @headers_dict = headers_dict
+  end
+  
+  def method_missing(name)
+    @row[@headers_dict[name]]
+  end
+end
 class RubyCsv
   include ActsAsCsv
   acts_as_csv
+
+  def each(&block)
+    @csv_contents.each do |csv_row|
+      block.call csv_row
+    end
+  end
+    
 end
 
+if __FILE__ == $0
+  lines = [
+    'one, two',
+    'lions, tigers',
+    'flions, ftigers'
+  ]
+  csv = RubyCsv.new(lines)
+  csv.each {|row| puts row.one}
+end
